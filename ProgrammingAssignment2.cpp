92394c60cc3bf3924e471b7c3211c9ef1e6f8940
Justin Smith <smithj359@macomb.edu>
2022-06-03 21:06:23 -0400

    Start of programming assignment 2

--- ProgrammingAssignment2.cpp
+++ ProgrammingAssignment2.cpp
@@ -1,20 +1,36 @@
-// ProgrammingAssignment2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Programming Asignment 2
+// By Justin Smith
 
 #include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+const double CONTAINER_QUANTITY = 2.76;
+const double CONTAINER_COST = 4.12;
+const double CONTAINER_PROFIT = 1.45;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int cheeseProduced;
+	double numContainers;
+	string s1;
+
+	cout << "*************************************************************" << endl;
+	cout << "***************Welcome to my Cheese Calculator***************" << endl;
+	cout << "*************************************************************" << endl;
+
+	cout << "Please enter the total numbor of kilograms of cheese produced: ";
+	cin >> cheeseProduced;
+
+	numContainers = cheeseProduced / CONTAINER_QUANTITY;
+
+	cout << "The number of containers to hold the cheese is: " << numContainers << endl;
+
+	string s1 = "The cost of producing " + to_string(numContainers) + " container(s) of cheese is: ";
+	cout << setw(5) << left << s1 << setw(5) << right << '$' << numContainers * CONTAINER_COST << endl;
+
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
