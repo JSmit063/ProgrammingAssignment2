71726d70fcc5406a5b53196138810d290a682201
Justin Smith <smithj359@macomb.edu>
2022-06-05 19:58:56 -0400

    Final program

--- ProgrammingAssignment2.cpp
+++ ProgrammingAssignment2.cpp
@@ -1,5 +1,6 @@
 // Programming Asignment 2
 // By Justin Smith
+// 6/5/22
 
 #include <iostream> // Need for use of I/O
 #include <iomanip> // Need for format manipulators
@@ -20,28 +21,31 @@ int main()
 	string s1;
 	string s2;
 
-	cout << "**********************************************************************************" << endl;
-	cout << "************************* Welcome to my Cheese Calculator ************************" << endl;
-	cout << "**********************************************************************************" << endl << endl;
+	cout << "***********************************************************************************" << endl;
+	cout << "*************************** Welcome to my Cheesy Program **************************" << endl;
+	cout << "***********************************************************************************" << endl << endl;
 
 	// Prompt the user to enter an integer amount of kg of cheese produced
 	cout << "Please enter the total numbor of kilograms of cheese produced: ";
-	cin >> cheeseProduced; // Assignment to the cheeseProduced variable
+	cin >> cheeseProduced;
 
 	// Find the total number of containers for the amount of cheese produced
 	// Output to user the amount
 	numContainers = cheeseProduced / CONTAINER_QUANTITY;
-	cout << "The number of containers to hold the cheese is: " << numContainers << endl;
+	cout << "The number of containers to hold the cheese is: " << fixed << setprecision(0) << numContainers << endl;
 
 	// Need to use a string here for the cost of producing cheese amount inputed
-	//
-	s1 = "The cost of producing " + to_string(numContainers) + " container(s) of cheese is: ";
-	cout << setw(25) << left << s1 << setw(25) << right << '$' << numContainers * CONTAINER_COST << endl;
+	// Line 38 is casting numContainer to an int, adding the .5 to compensate for truncation
+	// and then casting the variable to a string
+	s1 = "The cost of producing " + to_string(static_cast<int>(numContainers + .5)) + " container(s) of cheese is: ";
+	cout << setw(25) << left << s1 << setw(24) << right << '$' << setprecision(2) << static_cast<int>(numContainers + .5) * CONTAINER_COST << endl;
 
-	s2 = "The profit from producing " + to_string(numContainers) + " container(s) do cheese is: ";
-	cout << setw(25) << left << s2 << setw(25) << right << '$' << numContainers * CONTAINER_PROFIT << endl;
+	// Line 44 is casting numContainer to an int, adding the .5 to compensate for truncation
+	// and then casting the variable to a string
+	s2 = "The profit from producing " + to_string(static_cast<int>(numContainers + .5)) + " container(s) do cheese is: ";
+	cout << setw(25) << left << s2 << setw(20) << right << '$' << setprecision(2) << static_cast<int>(numContainers + .5) * CONTAINER_PROFIT << endl;
 
-	system("pause");
+	system("pause"); // system pause to see results
 
 	return 0;
 }
